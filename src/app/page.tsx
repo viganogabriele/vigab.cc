16d0275c784186c20a3034280ca213d0d72c47e8
Gabriele Viganò <infogabrielevigano@gmail.com>
2025-12-13 22:34:49 +0100

    feat(redirect): Update homepage redirect to viganogabriele.com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation"
 
 export default async function HomePage() {
   // Redirect to the Home page
-  redirect("https://polinetwork.org")
+  redirect("https://viganogabriele.com")
 }
